e79951063796c42e9c9e91a7d4a1c33f48bf7001
AzarayAOS <AzarayAOS@gmail.com>
2017-03-17 19:01:40 +0300

    Add files via upload

--- Point_Neiron_Net/Form1.Designer.cs
+++ Point_Neiron_Net/Form1.Designer.cs
@@ -37,23 +37,29 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Location = new System.Drawing.Point(12, 522);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 27);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Запуск";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // timer1
             // 
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Bisque;
             this.pictureBox1.Location = new System.Drawing.Point(12, 8);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1110, 508);
+            this.pictureBox1.Size = new System.Drawing.Size(1100, 500);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
@@ -67,6 +73,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Point";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- Point_Neiron_Net/Form1.cs
+++ Point_Neiron_Net/Form1.cs
@@ -12,34 +12,72 @@ namespace Point_Neiron_Net
 {
     public partial class Form1 : Form
     {
-        private float x, y;
-        private bool f;
+        public Ellips[] El;
+        private const int n = 10; // количество шариков на простарнстве каждого цвета
+        private Random r;       // генератор начального положения
+        private int GrafPointW = 1100, GrafPointH = 500; // ширина и высота области рисования в пикселях
 
         public Form1()
         {
-            f = false;
-            x = 4;
-            y = 3;
             InitializeComponent();
+
+            r = new Random();
+            El = new Ellips[n * 3];
+
+            GrafPointH = pictureBox1.Height;
+            GrafPointW = pictureBox1.Width;
+
+            Parallel.For(0, n, i => { El[i] = new Ellips(StartX(), StartY(), ChangVector(), ChangVector(), Color.Red); });
+            Parallel.For(n, n * 2, i => { El[i] = new Ellips(StartX(), StartY(), ChangVector(), ChangVector(), Color.Orange); });
+            Parallel.For(n * 2, n * 3, i => { El[i] = new Ellips(StartX(), StartY(), ChangVector(), ChangVector(), Color.Green); });
+        }
+
+        // задание движения
+        protected double ChangVector()
+        {
+            return (r.Next(-10, 10) / 3);
+        }
+
+        // для определения начальной координаты круга по Х
+        public double StartX()
+        {
+            return r.Next(100, GrafPointW);
+        }
+
+        // для определения начальной координаты по У
+        public double StartY()
+        {
+            return r.Next(50, GrafPointH);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Enabled = !timer1.Enabled;
-
-            f = !f;     // включение/отключение
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            if (f)
-            {
-                e.Graphics.Clear(Color.White);
+            e.Graphics.Clear(Color.White);
 
-                x += 0.5f;
-                y += 0.3f;
-                e.Graphics.FillEllipse(new SolidBrush(Color.Red), new Rectangle((int)x, (int)y, 50, 50));
+            // поскольку объекты никак не зависят друг от друга, то их следующее положение просчитываем паралельно
+
+            Parallel.For(0, n * 3, i =>
+                    { El[i].Draw(e, GrafPointW, GrafPointH); });
+
+            for (var i = 0; i < n * 3; i++)
+            {
+                e.Graphics.FillEllipse(new SolidBrush(Color.Black), new Rectangle((int)(El[i].GetX()) - El[i].GetThicnkness(), (int)(El[i].GetY()) - El[i].GetThicnkness(), El[i].GetRadius() * 2 + El[i].GetThicnkness() * 2, El[i].GetRadius() * 2 + El[i].GetThicnkness() * 2));
+                e.Graphics.FillEllipse(new SolidBrush(El[i].GetColor()), new Rectangle((int)(El[i].GetX()), (int)(El[i].GetY()), El[i].GetRadius() * 2, El[i].GetRadius() * 2));
             }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            GrafPointH = this.Height - 100;
+            GrafPointW = this.Width - 50;
+
+            this.pictureBox1.Height = GrafPointH;
+            this.pictureBox1.Width = GrafPointW;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
